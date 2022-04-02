1c6029c89881f6562521147f21a4a108f7522eb2
JosephJamesCoop <cooper_3869@live.com>
2022-04-02 10:01:25 -0700

    added about section

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
 import React from "react";
 
 import About from "./components/About";
-import ContactForm from "./components/Contact";
-import MyWork from "./components/MyWork";
-import Nav from "./components/Nav";
-import Resume from "./components/Resume";
+// import ContactMe from "./components/Contact";
+// import MyWork from "./components/MyWork";
+// import Nav from "./components/Nav";
+// import Resume from "./components/Resume";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    {/* <Nav></Nav> */}
+    <main>
+      <><About></About></>
+      {/* <><ContactMe></ContactMe></>
+      <><MyWork></MyWork></>
+      <><Resume></Resume></> */}
+    </main>
+  </div>
   );
 }
 
